7b292e0a339d52426ef1e666d5b744afd110920c
Trey-Rubino <122949187+Trey-Rubino@users.noreply.github.com>
2023-04-08 20:09:31 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -22,9 +22,9 @@ const handleMouseDownOnce = () => {
 window.addEventListener('keydown', handleFirstTab)
 
 const backToTopButton = document.querySelector(".back-to-top");
-let isBackToTopRendered = false;
+var isBackToTopRendered = false;
 
-let alterStyles = (isBackToTopRendered) => {
+var alterStyles = (isBackToTopRendered) => {
   backToTopButton.style.visibility = isBackToTopRendered ? "visible" : "hidden";
   backToTopButton.style.opacity = isBackToTopRendered ? 1 : 0;
   backToTopButton.style.transform = isBackToTopRendered
@@ -42,7 +42,70 @@ window.addEventListener("scroll", () => {
   }
 });
 
+/* --------------------------------------- */
+/* ----- Reveal on Scroll ----- */
+/* --------------------------------------- */
 
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal"); 
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
 
+window.addEventListener("scroll", reveal);
 
+/* --------------------------------------- */
+/* ----- Progress Bars  ----- */
+/* --------------------------------------- */
 
+function progress(idName) {
+  var temp;
+  switch(idName) {
+    case 1 : {
+      temp = 90;
+      break;
+    }
+    case 2 : {
+      temp = 60;
+      break;
+    }
+    case 3 : {
+      temp = 80;
+      break;
+    }
+    case 4 : {
+      temp = 20;
+      break;
+    }
+    default : {
+      temp = 50;
+      break;
+    }
+  }
+  var bars = document.getElementById(idName);
+  var width = 1;
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (width >= temp) {
+      clearInterval(id);
+    } else {
+      width++;
+      bars.style.width = width + "%";
+      bars.innerHTML = width + "%";
+    }
+  }
+}
+
+function moveProgressBars() {
+  for (var i = 1; i <= 5; i++) {
+    progress(i);
+  }
+}
